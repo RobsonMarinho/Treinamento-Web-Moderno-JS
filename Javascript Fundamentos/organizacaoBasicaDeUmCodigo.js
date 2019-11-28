a7844f9197479daafbed1609827c1a0a0f721587
Robson Marinho <robson_luz15@hotmail.com>
2019-11-27 21:15:28 -0300

    Organização dbásica de um código JS

---
+++ Javascript Fundamentos/organizacaoBasicaDeUmCodigo.js
@@ -0,0 +1,12 @@
+/*Como JS é organizado em sentença de 
+códigos, que podem ou não terminar em vírgula, 
+e é organizado em blocos de códigos
+*/
+
+console.log("Senteça de código");
+{
+    {
+        console.log("Senteça de código");
+        console.log("Senteça de código");
+    }
+}
