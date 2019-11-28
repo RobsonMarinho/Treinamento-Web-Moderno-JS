c8c8b44c998c69c337f2c861a51a17707fdb3200
Robson Marinho <robson_luz15@hotmail.com>
2019-11-27 21:28:05 -0300

    Comentários JS

---
+++ Javascript Fundamentos/comentario.js
@@ -0,0 +1,13 @@
+// Comentário de uma linha
+console.log('Linha 1');
+
+//Comentário de múltiplas linhas
+console.log('Linha 2')
+
+/*Cometário
+*de
+*múltiplss
+*linhas
+*/
+
+console.log('Linha 3')
