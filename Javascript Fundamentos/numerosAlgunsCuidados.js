8db263b3b21dac7b6bdf8442599bb36822b05433
Robson Marinho <robson_luz15@hotmail.com>
2019-12-15 17:37:15 -0300

    Math

--- Javascript Fundamentos/numerosAlgunsCuidados.js
+++ Javascript Fundamentos/numerosAlgunsCuidados.js
@@ -4,3 +4,4 @@ console.log("Show!" * 2); /*não funciona por reconhecer ser string*/
 console.log(0.1 + 0.7);
 /*console.log(10.toString()); /*gera uma excessão*/  
 console.log((10).toFixed(2));/*divide 10/2*/
+console.log();
