1e52308d59da5fc85219c0d4bbe02ac6dfa76f0a
Robson Marinho <robson_luz15@hotmail.com>
2019-12-15 17:30:15 -0300

    Alguns cuidados com números no JS

---
+++ Javascript Fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,6 @@
+console.log(7/0); /*vai dar infinito*/
+console.log("10" / 2); /*ele divide e o resultado é 5*/
+console.log("Show!" * 2); /*não funciona por reconhecer ser string*/
+console.log(0.1 + 0.7);
+/*console.log(10.toString()); /*gera uma excessão*/  
+console.log((10).toFixed(2));/*divide 10/2*/
