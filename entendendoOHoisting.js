2359528955da191d07d20f215908e565a9f9bc6e
Robson Marinho <robson_luz15@hotmail.com>
2020-02-11 23:25:28 -0300

    Entendo o conceito na prática do Hoisting

---
+++ entendendoOHoisting.js
@@ -0,0 +1,9 @@
+console.log('a = ', a); // Variável não declarada
+var a = 2;
+console.log('a =', a);
+
+console.log('a = ', a); // Let não funciona
+let a = 2;
+console.log('a =', a);
+
+//Hoisting: joga a declaração das variáveis definidas com o var pra cima
