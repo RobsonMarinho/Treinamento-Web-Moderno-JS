6883df2e99eed0b542e2c7adab4a42fbdf8a1c2c
Robson Marinho <robson_luz15@hotmail.com>
2020-10-04 01:38:44 -0300

    Estrutura de controle IF #01

---
+++ Section 04 - Javascript_Estrutura de Controles/usando_a_estrutura_IF_01.js
@@ -0,0 +1,23 @@
+function soBoasNoticia(nota) {
+            if (nota >= 7) {
+                        console.log("Aprovado com " + nota);
+            }
+}
+
+soBoasNoticia(8.1);
+soBoasNoticia(6.1);
+
+function seForVerdadeEuFalo(valor) {
+            if(valor) {
+                        console.log('É verdade...' + valor);
+            }
+}
+
+seForVerdadeEuFalo();
+seForVerdadeEuFalo(null);
+seForVerdadeEuFalo(undefined);
+seForVerdadeEuFalo(NaN);
+seForVerdadeEuFalo(``);
+seForVerdadeEuFalo(0);
+seForVerdadeEuFalo(1);
+seForVerdadeEuFalo(` `);
